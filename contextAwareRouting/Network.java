bd0dfce9b5aedfa43b3997bd8487560d7934f5ab
Kevin Lee <lee.kev@me.com>
2013-03-05 07:27:13 +0000

    git-svn-id: https://subversion.assembla.com/svn/elec476/src@12 4db04919-9836-40c1-aee8-a115cec00ef4

---
+++ contextAwareRouting/Network.java
@@ -0,0 +1,21 @@
+package contextAwareRouting;
+
+public class Network {
+	
+	private boolean[][] adjMat; //Adjacency Matrix
+	
+	public Network( boolean[][] inputAdjMat){
+		
+		setAdjMat(inputAdjMat);
+		
+	}
+
+	void setAdjMat(boolean[][] adjMat){
+		this.adjMat = adjMat;
+	}
+	
+	boolean[][] getAdjMat(){
+		return adjMat;
+	}
+	
+}
