fc4b3a7cea1d0c831adf1eae81f8fe8f02aa9046
Nikita Vakulin <zippypic@gmail.com>
2013-03-05 22:30:47 +0000

    Added the discrete time as well as the creation of arrival events
    git-svn-id: https://subversion.assembla.com/svn/elec476/trunk@22 4db04919-9836-40c1-aee8-a115cec00ef4

--- src/contextAwareRouting/RelayNode.java
+++ src/contextAwareRouting/RelayNode.java
@@ -4,39 +4,19 @@ import java.util.LinkedList;
 
 public class RelayNode {
 	
-	private  LinkedList <Request>queue;
+	private  LinkedList <Request> Queue;
 	private int processing;
 	private int xpos;
 	private int ypos;
 	
-	public RelayNode(LinkedList<Request> inputQueue, int inputProcessing, int inputXpos, int inputYpos){
+	public RelayNode(int inputXpos, int inputYpos){
 		
-		setQueue(inputQueue);
-		setProcessing(inputProcessing);
-		setXpos(inputXpos);
-		setYpos(inputYpos);
+		Queue = new LinkedList<Request>();
+		processing = 0;
+		xpos = inputXpos;
+		ypos = inputYpos;
 		
-	}
-	
-	void setQueue(LinkedList<Request> queue){
-		this.queue = queue; 
-	}
-	
-	void setProcessing(int processing){
-		this.processing = processing;
-	}
-	
-	public void setXpos(int xpos) {
-		this.xpos = xpos;
-	}
-	
-	public void setYpos(int ypos) {
-		this.ypos = ypos;
 	}	
-	
-	LinkedList<Request> getQueue(){
-		return queue;
-	}
 	
 	int getProcessing(){
 		return processing;
--- src/contextAwareRouting/Test.java
+++ src/contextAwareRouting/Test.java
@@ -3,11 +3,11 @@ package contextAwareRouting;
 public class Test {
 
 	public static void main(String[] args) {
-		RandomNumGen generator1 = new RandomNumGen(0,10);
-		System.out.println(generator1.uniformList(10).toString());
+		RandomNumGen generator1 = new RandomNumGen();
 		
-		RandomNumGen generator2 = new RandomNumGen();
-		System.out.println(generator2.poissonArrivalTimesList(2, 10));
+		System.out.println(generator1.uniformList(10, 0 ,10).toString());
+	
+		System.out.println(generator1.poissonList(2, 10));
 	
 	}
 
