ab83137b713c5ae051ce42cc278cb2ff25807db8
Kevin Lee <lee.kev@me.com>
2013-03-28 18:57:22 +0000

    March 28
    git-svn-id: https://subversion.assembla.com/svn/elec476/trunk/src@29 4db04919-9836-40c1-aee8-a115cec00ef4

--- contextAwareRouting/Request.java
+++ contextAwareRouting/Request.java
@@ -3,7 +3,7 @@ package contextAwareRouting;
 import java.util.EnumMap;
 
 public class Request {
-	
+
 	//Request description attributes
 	private int sourceNodeID;
 	private int destinationNodeID;
@@ -12,12 +12,12 @@ public class Request {
 	private int app;
 	private enum State{NEW, QUEUE, INSERVICE, ARRIVED, DROPPED};
 	private State state;
-	
+
 	//Request simulation data, array of int [start time, queue time, system time, dropped]
 	private enum DataType{STARTTIME, QUEUETIME, SYSTEMTIME, DROPPEDFLAG};
 	private EnumMap<DataType,Integer> Data = new EnumMap<DataType,Integer>(DataType.class);
-//	private int[] Data = new int[4];
-	
+	//	private int[] Data = new int[4];
+
 	public Request(int sourceNodeID, int destinationNodeID, int app, int tick){
 		//setup
 		this.sourceNodeID = sourceNodeID;
@@ -28,10 +28,10 @@ public class Request {
 		Data.put(DataType.QUEUETIME, 0);
 		Data.put(DataType.SYSTEMTIME, 0);
 		Data.put(DataType.DROPPEDFLAG, 0);
-		
+
 		currentNodeID = sourceNodeID;
 	}
-	
+
 	public void tick(int tick){
 		//switch case for counting time depending on the state of the request
 		switch (state) {
@@ -52,10 +52,10 @@ public class Request {
 		default:
 			break;
 		}
-		
+
 		//other tracking code can go here possibly even with tracking capability due to tick being passed in
 	}
-	
+
 	public void setState(int newState){
 		//switch case for changing state
 		switch (newState) {
@@ -73,7 +73,7 @@ public class Request {
 			//throw new Exception("Invalid request state");
 		}
 	}
-	
+
 	public void setState(int newState, int nodeID){
 		//overloaded method for changing state to QUEUE
 		if (newState==1){
@@ -91,7 +91,7 @@ public class Request {
 	public int getDestinationNodeID() {
 		return destinationNodeID;
 	}
-	
+
 	public int getApp() {
 		return app;
 	}
